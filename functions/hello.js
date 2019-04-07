dba48e39cdd4bea06180182b175b13ec973be4d6
Andrew Hamilton <andrew.hamilton@gmail.com>
2019-04-07 11:05:39 -0400

    api

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,13 @@
+var request = require("request");
+
 exports.handler = function(event, context, callback) {
+
+  var url = "https://api.netlify.com/api/v1/forms/wonderful-bassi-177437/submissions/?access_token=" + process.env.API_AUTH;
+
+  request(url, function(err, response, body){
     callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-    });
+      statusCode: 200,
+      body
+    })
+  })
 }
