34f4ceb5e58955d780a63430370dd63dca3f874b
Andrew Hamilton <andrew.hamilton@gmail.com>
2019-04-07 12:42:11 -0400

    error handler

--- functions/hello.js
+++ functions/hello.js
@@ -4,9 +4,13 @@ exports.handler = function(event, context, callback) {
   var url = "https://api.netlify.com/api/v1/forms/wonderful-bassi-177437/submissions?access_token=" + process.env.API_AUTH;
 
   request(url, function(err, response, body){
-    callback(null, {
-      statusCode: 200,
-      body
-    })
+    if (err) {
+       callback(err)
+    } else {
+      callback(null, {
+        statusCode: 200,
+        body
+      })
+    }
   })
 }
