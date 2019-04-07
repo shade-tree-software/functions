029c6fc916de5f612f4636f6f53e2696c4381d78
Andrew Hamilton <andrew.hamilton@gmail.com>
2019-04-07 12:45:09 -0400

    let request

--- functions/hello.js
+++ functions/hello.js
@@ -3,6 +3,7 @@ exports.handler = function(event, context, callback) {
 
   var url = "https://api.netlify.com/api/v1/forms/wonderful-bassi-177437/submissions?access_token=" + process.env.API_AUTH;
 
+  let request = require('request')
   request(url, function(err, response, body){
     if (err) {
        callback(err)
