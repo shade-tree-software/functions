08da0391b3513ac569cf975f8023e5d1caacbf98
Andrew Hamilton <andrew.hamilton@gmail.com>
2019-04-07 12:34:53 -0400

    syntax error

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,7 @@
 
 exports.handler = function(event, context, callback) {
 
-  var url = "https://api.netlify.com/api/v1/forms/wonderful-bassi-177437/submissions/?access_token=" + process.env.API_AUTH;
+  var url = "https://api.netlify.com/api/v1/forms/wonderful-bassi-177437/submissions?access_token=" + process.env.API_AUTH;
 
   request(url, function(err, response, body){
     callback(null, {
